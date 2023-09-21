322c2b93d3349427ebc187a6ffcdb5c4ffcf40b0
ahmed.asif <ahmed.asif@bjitgroup.com>
2023-09-21 18:48:17 +0600

    product details updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Common;
+use App\Models\Attribute;
 use App\Models\Product;
 use App\Models\ProductDiscount;
 use App\Models\ProductImage;
@@ -174,12 +175,39 @@ class ProductController extends Controller
         //dd($product_attributes);
         $size_array = array();
         $color_array = array();
+        $other_array = array();
+        $main_array = array();
+
+        $all_attributes = Attribute::all();
+       
+        foreach ($all_attributes as $key => $value) {
+          if($value->attribute_name == 'Size'){
+            $main_array['size'] = json_decode($value->attribute_value, true);
+          } elseif($value->attribute_name == 'Color'){
+            $main_array['color'] = json_decode($value->attribute_value, true);
+          } else {
+            $main_array['other'] = json_decode($value->attribute_value, true);
+          }
+        }
+
+        //dd($main_array['size']);
 
         foreach ($product_attributes as $pakey => $pavalue) {
             $parray = explode("+", $pavalue['attribute_title']);
+            foreach ($parray as $key => $value) {
+              //echo $value.'<br>';
+              if(in_array(trim($value), $main_array['size'])){
+                $size_array[] = trim($value); 
+              } elseif(in_array(trim($value), $main_array['color'])){
+                $color_array[] = trim($value);
+              } else {
+                $other_array[] = trim($value);
+              }
+            }
             
         }
-        //exit();
+        $size_data = array_unique($size_array);
+        $color_data = array_unique($color_array);
 
         $gallery_images = json_decode($product_details->product_images->gallery_images, true);
         //dd($gallery_images);
@@ -187,6 +215,26 @@ class ProductController extends Controller
         foreach ($gallery_images as $key => $value) {
             $gallery_image_url = asset('/uploads/products/'.$pid.'/gallery_images/'.$value);
             $gallery_html .= '<div class="product-image-thumb"><img src="'.$gallery_image_url.'" alt="Product Image"></div>';
+        }
+
+        $color_html = '';
+        foreach ($color_data as $ckey => $cvalue) {
+          $class_name = 'text-'.strtolower($cvalue);
+          $color_html .= '<label class="btn btn-default text-center active">
+                  <input type="radio" name="color_option" id="color_option_a'.$ckey.'" autocomplete="off" checked>
+                  '.$cvalue.'
+                  <br>
+                  <i class="fas fa-circle fa-2x '.$class_name.'"></i>
+                </label>';
+        }
+
+        $size_html = '';
+        foreach ($size_data as $skey => $svalue) {
+          $size_html .= '<label class="btn btn-default text-center">
+                  <input type="radio" name="color_option" id="color_option_'.$skey.'" autocomplete="off">
+                  <span class="text-xl">'.$svalue.'</span>
+                  <br>
+                </label>';
         }
 
         $html ='<section class="content">
@@ -210,64 +258,12 @@ class ProductController extends Controller
               <hr>
               <h4>Available Colors</h4>
               <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-default text-center active">
-                  <input type="radio" name="color_option" id="color_option_a1" autocomplete="off" checked>
-                  Green
-                  <br>
-                  <i class="fas fa-circle fa-2x text-green"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a2" autocomplete="off">
-                  Blue
-                  <br>
-                  <i class="fas fa-circle fa-2x text-blue"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a3" autocomplete="off">
-                  Purple
-                  <br>
-                  <i class="fas fa-circle fa-2x text-purple"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a4" autocomplete="off">
-                  Red
-                  <br>
-                  <i class="fas fa-circle fa-2x text-red"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a5" autocomplete="off">
-                  Orange
-                  <br>
-                  <i class="fas fa-circle fa-2x text-orange"></i>
-                </label>
+                '.$color_html.'
               </div>
 
-              <h4 class="mt-3">Size <small>Please select one</small></h4>
+              <h4 class="mt-3">Available Sizes</h4>
               <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b1" autocomplete="off">
-                  <span class="text-xl">S</span>
-                  <br>
-                  Small
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b2" autocomplete="off">
-                  <span class="text-xl">M</span>
-                  <br>
-                  Medium
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b3" autocomplete="off">
-                  <span class="text-xl">L</span>
-                  <br>
-                  Large
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b4" autocomplete="off">
-                  <span class="text-xl">XL</span>
-                  <br>
-                  Xtra-Large
-                </label>
+                '.$size_html.'
               </div>
 
               <div class="bg-gray py-2 px-3 mt-4">
@@ -292,7 +288,7 @@ class ProductController extends Controller
               </div>
             </nav>
             <div class="tab-content p-3" id="nav-tabContent">
-              <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh, congue et ante et, commodo mattis lacus. Duis varius finibus purus sed venenatis. Vivamus varius metus quam, id dapibus velit mattis eu. Praesent et semper risus. Vestibulum erat erat, condimentum at elit at, bibendum placerat orci. Nullam gravida velit mauris, in pellentesque urna pellentesque viverra. Nullam non pellentesque justo, et ultricies neque. Praesent vel metus rutrum, tempus erat a, rutrum ante. Quisque interdum efficitur nunc vitae consectetur. Suspendisse venenatis, tortor non convallis interdum, urna mi molestie eros, vel tempor justo lacus ac justo. Fusce id enim a erat fringilla sollicitudin ultrices vel metus. </div>
+              <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"> '.$product_details->long_description.'</div>
               <div class="tab-pane fade" id="product-comments" role="tabpanel" aria-labelledby="product-comments-tab"> Vivamus rhoncus nisl sed venenatis luctus. Sed condimentum risus ut tortor feugiat laoreet. Suspendisse potenti. Donec et finibus sem, ut commodo lectus. Cras eget neque dignissim, placerat orci interdum, venenatis odio. Nulla turpis elit, consequat eu eros ac, consectetur fringilla urna. Duis gravida ex pulvinar mauris ornare, eget porttitor enim vulputate. Mauris hendrerit, massa nec aliquam cursus, ex elit euismod lorem, vehicula rhoncus nisl dui sit amet eros. Nulla turpis lorem, dignissim a sapien eget, ultrices venenatis dolor. Curabitur vel turpis at magna elementum hendrerit vel id dui. Curabitur a ex ullamcorper, ornare velit vel, tincidunt ipsum. </div>
               <div class="tab-pane fade" id="product-rating" role="tabpanel" aria-labelledby="product-rating-tab"> Cras ut ipsum ornare, aliquam ipsum non, posuere elit. In hac habitasse platea dictumst. Aenean elementum leo augue, id fermentum risus efficitur vel. Nulla iaculis malesuada scelerisque. Praesent vel ipsum felis. Ut molestie, purus aliquam placerat sollicitudin, mi ligula euismod neque, non bibendum nibh neque et erat. Etiam dignissim aliquam ligula, aliquet feugiat nibh rhoncus ut. Aliquam efficitur lacinia lacinia. Morbi ac molestie lectus, vitae hendrerit nisl. Nullam metus odio, malesuada in vehicula at, consectetur nec justo. Quisque suscipit odio velit, at accumsan urna vestibulum a. Proin dictum, urna ut varius consectetur, sapien justo porta lectus, at mollis nisi orci et nulla. Donec pellentesque tortor vel nisl commodo ullamcorper. Donec varius massa at semper posuere. Integer finibus orci vitae vehicula placerat. </div>
             </div>
